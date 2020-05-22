85a102733f2fe77807cd85bb358cee34d02949c7
Yili He <eli.he999@hotmail.com>
2020-05-22 18:15:37 +0800

    Update example

--- testlogin.py
+++ testlogin.py
@@ -1,3 +1,4 @@
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from webium.controls.link import Link
@@ -17,26 +18,18 @@ class ClassicLoginPage(BasePage):
     button = wordlist.return_find_elem('loginsubmit')
 
 class MainPage(BasePage):
-    label_list = Finds(by=By.XPATH, value='//*[@class="data-list"]/tbody/tr/td/div/table/tbody/tr[@class="table-row"]/td[1]//div[@class="gwt-HTML last"]')
     ver_label = Find(by=By.XPATH, value='//*[@id="verNo"]/div')
     reboot_link_btn = Find(by=By.LINK_TEXT, value="Reboot")
     reboot_ok_btn = Find(by=By.XPATH, value="//button[@class='button green']")
-    
-def find_coredump(label_list):
-    get_driver().implicitly_wait(5)
-    flag = 1
-    count_core = 0
-    for item in label_list:
-        if item.get_attribute("textContent").find("core") != -1:
-            flag = 0
-            count_core = count_core + 1
-    if flag == 1:
-        return 0
+
+class ClassicMainPage(BasePage):
+    topbaner = wordlist.return_find_elem('topBanner')
+    popoutwindow = wordlist.return_find_elem('popupContent')
+    resetpassword = wordlist.return_finds_elem('resetpassword')
 
 
-if __name__ == '__main__':
-    url = 'http://192.168.92.32'
-
+def reboot():
+    url = 'http://192.168.92.30'
     home_page = ClassicLoginPage(url=url)
     home_page.open()
     home_page.name_field.send_keys('admin')
@@ -47,7 +40,53 @@ if __name__ == '__main__':
     main_page.open()
     time.sleep(5)
     print(main_page.ver_label.get_attribute('textContent'))
-    find_coredump(main_page.label_list)
+    a = find_coredump(main_page.label_list)
+    print(a)
+    time.sleep(1)
+    main_page.reboot_link_btn.click()
+    wait.webiumWait().until(lambda browser: main_page.reboot_ok_btn)
+    main_page.reboot_ok_btn.click()
+    get_driver().quit()
+
+if __name__ == "__main__":
+    url = 'http://192.168.92.81'
+    home_page = ClassicLoginPage(url=url)
+    home_page.open()
+    home_page.name_field.send_keys('admin')
+    home_page.password_field.send_keys('123')
+    home_page.button.click()
+    #
+    temp_page = ClassicMainPage(url=url)
+    temp_page.open()
+    try:
+        wait.webiumWait().until(lambda browser: temp_page.popoutwindow)
+        for item in temp_page.popoutwindow:
+            if item.find_element_by_xpath('//div[@class="heading"]/div[@class="gwt-HTML"]').get_attribute('textContent') == "Admin Password":
+                try:
+                    i = 0
+                    for passinputbox in temp_page.resetpassword:
+                        try:
+                            if i == 0:
+                                pass
+                            else:
+                                Actions().move_n_click(passinputbox)
+                                passinputbox.send_keys("123")
+                        except BaseException as e:
+                            print("reset " + repr(e))
+                            pass
+                        finally:
+                            i = i + 1
+                except BaseException as e:
+                    print(repr(e))
+    except TimeoutException:
+        pass
+    #
+    main_page = MainPage(url=url+"#page:status_system_info")
+    main_page.open()
+    time.sleep(5)
+    main_page.refresh
+    label_list = Finds(by=By.XPATH, value='//*[@class="data-list"]/tbody/tr/td/div/table/tbody/tr[@class="table-row"]/td[1]//div[@class="gwt-HTML last"]')
+    print(main_page.ver_label.get_attribute('textContent'))
     time.sleep(1)
     main_page.reboot_link_btn.click()
     wait.webiumWait().until(lambda browser: main_page.reboot_ok_btn)
